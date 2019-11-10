1feafc180d10fa7b5ea31a39fa28dfe3ddbd55ee
jayinnn <jayin920805@gmail.com>
2019-11-10 11:30:53 +0800

    Add unity terrain gray image

--- png.py
+++ png.py
@@ -13,15 +13,15 @@ import cv2
 # In[21]:
 
 
-im = cv2.imread("Heightmap/Taiwan/23.800,121.048.png")
+im = cv2.imread("Heightmap/Unity_test.png")
 
 
 # In[22]:
 
 
 all_height = []
-for x in range(2000):
-    for y in range(4000):
+for x in range(257):
+    for y in range(257):
         pix = im[x][y]
         height = pix[0] + 256 * pix[1] + 256 ** 2 * pix[2]
         all_height.append(height)
@@ -37,7 +37,7 @@ all_height = np.array(all_height)
 # In[24]:
 
 
-all_height.resize(2000, 4000)
+all_height.resize(257, 257)
 
 
 # In[ ]:
--- 地形 - 複製/Assets/ToImage.cs
+++ 地形 - 複製/Assets/ToImage.cs
@@ -7,14 +7,15 @@ public class ToImage : MonoBehaviour {
 
     // Use this for initialization
     public Terrain terrain;
-	void Start () {
+    void Start () {
+        Color[] tmp = new Color[1];
+        Texture2D heightMap = new Texture2D(256, 256);
 		int w = terrain.terrainData.heightmapWidth;
 		int h = terrain.terrainData.heightmapHeight;
 		float[,] hm = terrain.terrainData.GetHeights(0, 0, w, h);
 		Color[] img = new Color[w*h];
 		float min = hm[0, 0];
 		float max = hm[0, 0];
-		float d;
 		for(int i=0;i<w;i++){
 			for(int j=0;j<h;j++){
 				if(hm[i, j] > max) max = hm[i, j]; 
@@ -23,11 +24,11 @@ public class ToImage : MonoBehaviour {
 		}
 		for(int i=0;i<w;i++){
 			for(int j=0;j<h;j++){
-				img[i*w+j] = new Color (hm[i, j] * 255 / max ,hm[i, j] * 255 / max, hm[i, j] * 255 / max);
+                tmp[0].r = tmp[0].g = tmp[0].b = hm[i, j] * 255 / max;
+                heightMap.SetPixel(i, j, Color.green);
 			}
 		}
-		BMPImage im;
-		// im.ImageData = img;
+        heightMap.Apply();
        
 		
 	}
