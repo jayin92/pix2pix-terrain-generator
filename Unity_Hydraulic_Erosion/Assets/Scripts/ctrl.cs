822c04145e4555b0f7a124098d2df11fb05c4e86
jayin92 <jayin920805@gmail.com>
2019-11-17 19:18:19 +0800

    Remove 地形-複製 & Add ToImage to ctrl.cs

--- Unity_Hydraulic_Erosion/Assets/Scripts/ctrl.cs
+++ Unity_Hydraulic_Erosion/Assets/Scripts/ctrl.cs
@@ -162,6 +162,41 @@ public class ctrl : MonoBehaviour {
             }
         }
     }
+    public void ToImage()
+    {
+        // public Terrain terrain;
+		int w = terrain.terrainData.heightmapWidth;
+		int h = terrain.terrainData.heightmapHeight;
+		Texture2D heightMap = new Texture2D(w, h);
+		float[,] hm = terrain.terrainData.GetHeights(0, 0, w, h);		
+		Color[] img = new Color[w*h];
+		float min = hm[0, 0];
+		float max = hm[0, 0];
+		for(int i=0;i<w;i++){
+			for(int j=0;j<h;j++){
+				if(hm[i, j] > max) max = hm[i, j]; 
+				if(hm[i, j] < min) min = hm[i, j];
+			}
+		}
+		// print(min);
+		// print(max);
+		for(int i=0;i<w;i++){
+			for(int j=0;j<h;j++){
+				img[i+j*h] = new Color(hm[i, j] / max, hm[i, j] / max, hm[i, j] / max);
+				// print(hm[i, j] * 255 / max);
+			}
+		}
+		heightMap.SetPixels(img);
+        heightMap.Apply();
+
+		FileStream fs = new FileStream(@"/home/jayinnn/scifair/test.png", FileMode.CreateNew);
+		fs.Write(heightMap.EncodeToPNG(), 0, heightMap.EncodeToPNG().Length);
+       
+		
+	}
+	
+}
+    }
 
 
 }
