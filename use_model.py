10d1526a2e603f339884e15539993cc8b9de3546
jayin92 <jayin920805@gmail.com>
2019-11-18 16:28:32 +0800

    Add use_model.py

---
+++ use_model.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataroot", help="path to test images")
+    parser.add_argument("--name", type=str, help="name of the experiment")
+    args = parser.parse_args()
+
+    name = args.name
+    dataroot = args.dataroot
+    print("---Remove all images in /tmp/test---")
+    os.system("rm -rf {}".format(os.path.join("heightmap", "tmp", "test")))
+    os.system("mkdir {}".format(os.path.join("heightmap", "tmp", "test")))
+    print("---Images stick with a black image---")
+    for file in os.listdir(dataroot):
+        try:
+            img = cv2.imread(os.path.join(dataroot, file))
+            size = img.shape[0]
+            tmp = [[0, 0, 0] for i in range(size)]
+            img_com = []
+            for i in range(size):
+                img_com.append(tmp)
+            img_com = np.array(img_com)
+            img = np.concatenate((img, img_com), axis=1)
+            cv2.imwrite(os.path.join("heightmap", "tmp", "test", "{}.png".format(file)), img)
+            plt.imshow(img)
+        except AttributeError:
+            print(file, "is not an image file")
+    print("---Complete---")
+    os.system("python ../pytorch-CycleGAN-and-pix2pix/test.py --dataroot {} --name {} --direction AtoB --model pix2pix".format(os.path.join("heightmap", "tmp", "test"), name))
