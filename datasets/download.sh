28255857273f35e9fc8dd1a401aef7f3291cf8f6
jayin92 <jayin920805@gmail.com>
2020-05-05 20:47:35 +0800

    datasets/download.sh now checks requirements is met now

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,3 +1,18 @@
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' wget|grep "install ok installed")
+echo Checking for somelib: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No wget. Setting up wget."
+  sudo apt-get --force-yes --yes install wget
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' unzip|grep "install ok installed")
+echo Checking for somelib: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No unzip. Setting up unzip."
+  sudo apt-get --force-yes --yes install unzip
+fi
+
+
 wget https://github.com/jayin92/scifair-datasets/releases/download/v1.0/heightmap.zip
 wget https://github.com/jayin92/scifair-datasets/releases/download/v1.0/perlin_for_test.zip
 wget https://github.com/jayin92/scifair-datasets/releases/download/v1.0/perlin_image.zip
